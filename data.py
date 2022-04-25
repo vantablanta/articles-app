ccbeb5af536f9d2194c92e3cc14bf3314542093b
vanta blanta <vantablanta@gmail.com>
2022-04-25 15:44:38 +0300

    data file added

---
+++ data.py
@@ -0,0 +1,12 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'name': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. \
+                    Explicabo nisi dolor quia, ipsum ab minima autem fugit molestias \
+                    inventore quas illo magnam facere, esse, illum doloremque modiodio\
+                    repudiandae veritatis.',
+            'author': "Brad Traversary"
+        }
+    ]
