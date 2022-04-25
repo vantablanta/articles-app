b42fb7a51fe76081a9efc524f2d7ea38ba11e50c
vanta blanta <vantablanta@gmail.com>
2022-04-25 15:32:57 +0300

    routing, and custome stylesheets

--- app/views.py
+++ app/views.py
@@ -4,5 +4,8 @@ from app import app
 @app.route('/')
 
 def index():
-    title = "Hello World"
-    return render_template('index.html', title = title)
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
