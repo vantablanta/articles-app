036b382c9ed8c67c3fdde010f86505124b438bdc
vanta blanta <vantablanta@gmail.com>
2022-04-26 07:54:15 +0300

    fetching data from local db.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
 from flask import render_template
 from app import app 
+from data import Articles
+
+Articles = Articles()
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
--- data.py
+++ data.py
@@ -7,6 +7,28 @@ def Articles():
                     Explicabo nisi dolor quia, ipsum ab minima autem fugit molestias \
                     inventore quas illo magnam facere, esse, illum doloremque modiodio\
                     repudiandae veritatis.',
-            'author': "Brad Traversary"
+            'author': "Brad Traversary",
+            'create_date':'04-25-2021'
+        },
+                {
+            'id': 2,
+            'name': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. \
+                    Explicabo nisi dolor quia, ipsum ab minima autem fugit molestias \
+                    inventore quas illo magnam facere, esse, illum doloremque modiodio\
+                    repudiandae veritatis.',
+            'author': "Michelle Njeri",
+            'create_date':'07-21-2022'
+        },
+        {
+            'id': 3,
+            'name': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. \
+                    Explicabo nisi dolor quia, ipsum ab minima autem fugit molestias \
+                    inventore quas illo magnam facere, esse, illum doloremque modiodio\
+                    repudiandae veritatis.',
+            'author': "Morris Muthumbi",
+            'create_date':'04-25-20121'
         }
     ]
+    return articles
